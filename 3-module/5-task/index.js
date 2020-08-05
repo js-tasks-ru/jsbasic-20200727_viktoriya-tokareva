47c5f1e7cbfdbb5d08dd87e6df0b40ed9e58b6a1
Viktoriya Tokareva <coke_alice@mail.ru>
2020-08-05 10:44:31 +0500

    Добавлены решения задач занятия №3

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -4,5 +4,14 @@
  * @returns {{min:number, max:number}}  объект
  */
 function getMinMax(str) {
-  // ваш код...
+	let newStr = str.split(/\s*,| \s*/);
+    
+	newStr.sort( (a, b) => a - b );
+	
+	let result = {
+      min: +newStr[0],
+      max: +newStr[newStr.length - 1],
+	};
+
+	return result;
 }
