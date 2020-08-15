27c62fe3d83b0d537a99edfc1db41674146af365
Viktoriya Tokareva <coke_alice@mail.ru>
2020-08-15 22:28:55 +0500

    Решения задач занятия №5

--- 5-module/1-task/index.js
+++ 5-module/1-task/index.js
@@ -1,3 +1,6 @@
 function hideSelf() {
-  // ваш код...
+  let button = document.querySelector('.hide-self-button');
+		button.addEventListener('click', function hiddenButton () {
+			button.hidden = true;
+		});
 }
--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,39 @@
 function initCarousel() {
-  // ваш код...
+  let carouselArrowRight = document.querySelector('.carousel__arrow_right');
+	let carouselArrowLeft = document.querySelector('.carousel__arrow_left');
+  let carouselInner = document.querySelector('.carousel__inner');
+  let slides = carouselInner.children;
+
+  let step = 0;
+  let slideWidth = carouselInner.firstElementChild.offsetWidth;
+
+  if (slides[step] == carouselInner.firstElementChild) {
+    carouselArrowLeft.style.display = 'none';
+  }
+  
+  function translateSlide() {
+    carouselInner.style.transform = 'translateX(' + (-slideWidth) * step + 'px)';
+
+    if (slides[step] == carouselInner.lastElementChild) {
+      carouselArrowRight.style.display = 'none';
+    } else {
+      carouselArrowRight.style.display = 'flex';
+    }
+
+    if (slides[step] == carouselInner.firstElementChild) {
+      carouselArrowLeft.style.display = 'none';
+    } else {
+      carouselArrowLeft.style.display = 'flex';
+    }
+  }
+
+  carouselArrowRight.addEventListener('click', function rightStep () {
+    step++;
+    translateSlide();
+  });
+  
+  carouselArrowLeft.addEventListener('click', function lestStep () {
+    step--;
+    translateSlide();
+	});
 }
